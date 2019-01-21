3d316b7c1ee75d850015dedaddb34ba2cd100ce4
Lena <elena.borisenko1991@gmail.com>
2019-01-21 15:54:12 +0000

    method indexOf created

--- src/index.js
+++ src/index.js
@@ -7,12 +7,28 @@ class LinkedList{
     size(){
         return this.length
     }
+    indexOf(value){
+        let currentNode = this.head;
+        let index = -1;
+        while(currentNode){
+            index++;
+            if(currentNode.value === value){
+                return index;
+            }
+            currentNode = currentNode.next;
+        }
+        return -1;
+        }
 }
+
 //element is an object with a value and next property
 class Node{
     constructor(value){
         this.value = value,
         this.next = null
     }
+    
 }
+
+
 
