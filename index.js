c2868cc57a492e40fa1c634f325bf8fcad01a120
Lena <elena.borisenko1991@gmail.com>
2019-01-21 12:22:40 +0000

    maters, start

---
+++ index.js
@@ -0,0 +1,18 @@
+class LinkedList{
+    constructor(){
+        this.head = null,
+        this.tail = null,
+        this.length = 0
+    }
+    size(){
+        return this.length
+    }
+}
+//element is an object with a value and next property
+class Node{
+    constructor(value){
+        this.value = value,
+        this.next = null
+    }
+}
+
